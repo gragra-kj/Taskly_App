23d20d0cd6b3c8ecf4dbd59d5e2861fb70a32106
gragra-kj <muthuigracia27@gmail.com>
2025-04-14 13:25:42 +0300

    signals

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 
+
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields=('id',)
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile,ProfileAdmin)
