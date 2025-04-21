b882c989d79fb04e3c11810e8b8ad00104cd80ae
gragra-kj <muthuigracia27@gmail.com>
2025-04-21 12:52:21 +0300

    Creating user viewsets

--- src/users/viewset.py
+++ src/users/viewset.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-
+from .serialize import UserSerializer
 class UserViewsets(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
