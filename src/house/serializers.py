69b1d13c56ebe53b6eb830fe3e08d04d823e88ad
gragra-kj <muthuigracia27@gmail.com>
2025-06-18 15:07:37 +0300

    house membership

--- src/house/serializers.py
+++ src/house/serializers.py
@@ -4,10 +4,12 @@ from .models import House
 
 class HouseSerializers(serializers.ModelSerializer):
     members_count=serializers.IntegerField(read_only=True)
+    members=serializers.HyperlinkedRelatedField(read_only=True,many=True,view_name='profile-detail')
+    manager=serializers.HyperlinkedRelatedField(read_only=True,many=False,view_name='profile-detail')
     
     
     class Meta:
         model=House
-        fields=['url','id','image','name','created_on','manager','description','members_count','points','completed_tasks_count','notcompleted_tasks_count']
+        fields=['url','id','image','name','created_on','manager','description','members_count','members','points','completed_tasks_count','notcompleted_tasks_count']
         read_only_fields=['points','completed_tasks_count','notcompleted_tasks_count']
         
